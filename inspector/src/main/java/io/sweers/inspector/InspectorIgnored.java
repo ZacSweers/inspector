69834c1c585eef9b7bf63110a0849bb09bd06494
Zac Sweers <pandanomic@gmail.com>
2018-02-19 22:53:15 +0100

    Allow annotations on fields for kotlin

--- inspector/src/main/java/io/sweers/inspector/InspectorIgnored.java
+++ inspector/src/main/java/io/sweers/inspector/InspectorIgnored.java
@@ -3,7 +3,8 @@ package io.sweers.inspector;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Retention(RUNTIME) @Target(METHOD) public @interface InspectorIgnored {}
+@Retention(RUNTIME) @Target({METHOD, FIELD}) public @interface InspectorIgnored {}
--- inspector/src/main/java/io/sweers/inspector/ValidatedBy.java
+++ inspector/src/main/java/io/sweers/inspector/ValidatedBy.java
@@ -3,10 +3,11 @@ package io.sweers.inspector;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Retention(RUNTIME) @Target(METHOD) public @interface ValidatedBy {
+@Retention(RUNTIME) @Target({METHOD, FIELD}) public @interface ValidatedBy {
   /**
    * @return an array of one or more {@link Validator} classes.
    */
